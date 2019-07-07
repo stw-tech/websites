fa1d4f2943f4325c08a658e64aa576f1bf38ccdc
ZengSQi <zengsqi199@gmail.com>
2019-07-07 16:35:25 +0800

    update project setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,16 +8,17 @@ module.exports = {
     parser: 'babel-eslint'
   },
   extends: [
+    'plugin:vue/essential',
     'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
     '@nuxtjs',
     'plugin:nuxt/recommended'
   ],
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['prettier'],
   // add your custom rules here
   rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
